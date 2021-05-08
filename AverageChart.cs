0e71d1487fa64d0c3202dd9083e1f569221e0ee6
Dale Hurtt <dalehurtt@gmail.com>
2021-05-07 18:02:52 -0700

    EMA added.

--- AverageChart.cs
+++ AverageChart.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace Charts {
+namespace Charts
+{
 
-    public class DailyAverageData {
+    public class DailyAverageData
+    {
 
         public DateTime Date { get; set; }
         public decimal Close { get; set; }
@@ -14,57 +16,70 @@ namespace Charts {
 
         public static readonly string Header = $"Date,Close,MA 1,MA 2,Signal";
 
-        public string ToCsv () {
+        public string ToCsv()
+        {
             return $"{Date:yyyy-MM-dd},{Close:F2},{MA1:F2},{MA2:F2},{Signal}";
         }
     }
 
-    public class SimpleAverageList : List<DailyAverageData> {
+    public class SimpleAverageList : List<DailyAverageData>
+    {
 
         public string Ticker { get; set; }
-        public int NumDays { get; set; }
+        public int NumDays1 { get; set; }
+        public int NumDays2 { get; set; }
 
-        public SimpleAverageList (List<DailyStockData> values, string ticker, int numDays1, int numDays2) {
+        public SimpleAverageList(List<DailyStockData> values, string ticker, int numDays1, int numDays2)
+        {
             Ticker = ticker;
-            NumDays = numDays1;
+            NumDays1 = numDays1;
+            NumDays2 = numDays2;
             int _i = -1, _j = -1, _k = -1;
-            try {
-                for (var i = 0; i < values.Count; i++) {
+            try
+            {
+                for (var i = 0; i < values.Count; i++)
+                {
                     _i = i;
-                    DailyStockData data = values [i];
+                    DailyStockData data = values[i];
                     // MA 1
                     int downTo1 = i >= (numDays1 - 1) ? i - (numDays1 - 1) : 0;
                     decimal acc1 = 0;
-                    for (var j = i; j >= downTo1; j--) {
+                    for (var j = i; j >= downTo1; j--)
+                    {
                         _j = j;
-                        acc1 += values [j].Close;
+                        acc1 += values[j].Close;
                     }
                     int divDays1 = i >= (numDays1 - 1) ? numDays1 : i + 1;
                     decimal avgPrice1 = acc1 / divDays1;
                     // MA 2
                     int downTo2 = i >= (numDays2 - 1) ? i - (numDays2 - 1) : 0;
                     decimal acc2 = 0;
-                    for (var k = i; k >= downTo2; k--) {
+                    for (var k = i; k >= downTo2; k--)
+                    {
                         _k = k;
-                        acc2 += values [k].Close;
+                        acc2 += values[k].Close;
                     }
                     int divDays2 = i >= (numDays2 - 1) ? numDays2 : i + 1;
                     decimal avgPrice2 = acc2 / divDays2;
                     // Create data record
                     string signal = string.Empty;
-                    if (data.Close >= avgPrice1 && data.Close >= avgPrice2) {
+                    if (data.Close >= avgPrice1 && data.Close >= avgPrice2)
+                    {
                         if (avgPrice1 >= avgPrice2) signal = "P12";
                         else signal = "P21";
                     }
-                    else if (avgPrice1 >= data.Close && avgPrice1 >= avgPrice2) {
+                    else if (avgPrice1 >= data.Close && avgPrice1 >= avgPrice2)
+                    {
                         if (data.Close >= avgPrice2) signal = "1P2";
                         else signal = "12P";
                     }
-                    else {
+                    else
+                    {
                         if (data.Close >= avgPrice1) signal = "2P1";
                         else signal = "21P";
                     }
-                    this.Add (new DailyAverageData {
+                    this.Add(new DailyAverageData
+                    {
                         Date = data.Date,
                         Close = data.Close,
                         MA1 = avgPrice1,
@@ -73,29 +88,136 @@ namespace Charts {
                     });
                 }
             }
-            catch (Exception ex) {
-                Utils.WriteToConsole ($"{Utils.ExToString (ex)}\ni: {_i} j: {_j} k: {_k}", true, "SimpleAverageList");
+            catch (Exception ex)
+            {
+                Utils.WriteToConsole($"{Utils.ExToString(ex)}\ni: {_i} j: {_j} k: {_k}", true, "SimpleAverageList");
             }
         }
 
-        public void OutputAsCsv (string filePath) {
-            try {
-                using StreamWriter file = new (filePath);
-                file.WriteLine (DailyAverageData.Header);
-                foreach (DailyAverageData value in this) {
-                    file.WriteLine (value.ToCsv ());
+        public void OutputAsCsv(string filePath)
+        {
+            try
+            {
+                using StreamWriter file = new(filePath);
+                file.WriteLine(DailyAverageData.Header);
+                foreach (DailyAverageData value in this)
+                {
+                    file.WriteLine(value.ToCsv());
                 }
             }
-            catch (Exception ex) {
-                Utils.WriteToConsole (Utils.ExToString (ex), true, "OutputAsCsv");
+            catch (Exception ex)
+            {
+                Utils.WriteToConsole(Utils.ExToString(ex), true, "OutputAsCsv");
             }
         }
     }
 
-    public class ExponentialAverageList : List<DailyAverageData> {
+    public class ExponentialAverageList : List<DailyAverageData>
+    {
 
         public string Ticker { get; set; }
-        public int NumDays { get; set; }
+        public int NumDays1 { get; set; }
+        public int NumDays2 { get; set; }
 
+        public ExponentialAverageList(List<DailyStockData> values, string ticker, int numDays1, int numDays2)
+        {
+            Ticker = ticker;
+            NumDays1 = numDays1;
+            NumDays2 = numDays2;
+            int _i = -1, _j = -1, _k = -1;
+            decimal mult1 = (2 / numDays1 + 1);
+            decimal mult2 = (2 / numDays2 + 1);
+            decimal yema1 = 0, yema2 = 0;
+            decimal ema1 = 0, ema2 = 0;
+            decimal sumema1 = 0, sumema2 = 0;
+            try
+            {
+                for (var i = 0; i < values.Count; i++)
+                {
+                    _i = i;
+                    DailyStockData data = values[i];
+                    // SMA 1
+                    int downTo1 = i >= (numDays1 - 1) ? i - (numDays1 - 1) : 0;
+                    decimal acc1 = 0;
+                    for (var j = i; j >= downTo1; j--)
+                    {
+                        _j = j;
+                        acc1 += values[j].Close;
+                    }
+                    int divDays1 = i >= (numDays1 - 1) ? numDays1 : i + 1;
+                    decimal avgPrice1 = acc1 / divDays1;
+                    // SMA 2
+                    int downTo2 = i >= (numDays2 - 1) ? i - (numDays2 - 1) : 0;
+                    decimal acc2 = 0;
+                    for (var k = i; k >= downTo2; k--)
+                    {
+                        _k = k;
+                        acc2 += values[k].Close;
+                    }
+                    int divDays2 = i >= (numDays2 - 1) ? numDays2 : i + 1;
+                    decimal avgPrice2 = acc2 / divDays2;
+                    // Calculate the EMA
+                    if (i == 0)
+                    {
+                        yema1 = avgPrice1;
+                        yema2 = avgPrice2;
+                    }
+                    ema1 = (data.Close - yema1) * mult1 + yema1;
+                    ema2 = (data.Close - yema2) * mult2 + yema2;
+                    sumema1 += ema1;
+                    sumema2 += ema2;
+                    yema1 = ema1;
+                    yema2 = ema2;
+                    avgPrice1 = sumema1 / divDays1;
+                    avgPrice2 = sumema2 / divDays2;
+                    // Create data record
+                    string signal = string.Empty;
+                    if (data.Close >= avgPrice1 && data.Close >= avgPrice2)
+                    {
+                        if (avgPrice1 >= avgPrice2) signal = "P12";
+                        else signal = "P21";
+                    }
+                    else if (avgPrice1 >= data.Close && avgPrice1 >= avgPrice2)
+                    {
+                        if (data.Close >= avgPrice2) signal = "1P2";
+                        else signal = "12P";
+                    }
+                    else
+                    {
+                        if (data.Close >= avgPrice1) signal = "2P1";
+                        else signal = "21P";
+                    }
+                    this.Add(new DailyAverageData
+                    {
+                        Date = data.Date,
+                        Close = data.Close,
+                        MA1 = avgPrice1,
+                        MA2 = avgPrice2,
+                        Signal = signal
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Utils.WriteToConsole($"{Utils.ExToString(ex)}\ni: {_i} j: {_j} k: {_k}", true, "ExponentialAverageList");
+            }
+        }
+
+        public void OutputAsCsv(string filePath)
+        {
+            try
+            {
+                using StreamWriter file = new(filePath);
+                file.WriteLine(DailyAverageData.Header);
+                foreach (DailyAverageData value in this)
+                {
+                    file.WriteLine(value.ToCsv());
+                }
+            }
+            catch (Exception ex)
+            {
+                Utils.WriteToConsole(Utils.ExToString(ex), true, "OutputAsCsv");
+            }
+        }
     }
 }
