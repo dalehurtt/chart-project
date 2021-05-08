23d11add67a8a5d191bd8f49380752604dfbe5a2
Dale Hurtt <dalehurtt@gmail.com>
2021-05-07 19:41:22 -0700

    Added MA and Vol Strength to report.

--- DailyReport.cs
+++ DailyReport.cs
@@ -18,8 +18,24 @@ namespace Charts {
         public decimal Buy { get; set; }
         public decimal Sell { get; set; }
         public bool FChanged { get; set; }
+        public decimal MA1 { get; set; }
+        public decimal MA2 { get; set; }
+        public string MASignal { get; set; }
+        public decimal VolStrength { get; set; } 
 
-        public static readonly string Header = "Ticker,Date,Close,P Signal,High/Low,Reversal,Changed,F Signal,Buy,Sell,Changed";
+        public List<AverageVolumeData> AddToReport (List<AverageVolumeData> values) {
+            AverageVolumeData last = values.Last();
+            VolStrength = last.Strength;
+            return values;
+        }
+
+        public List<DailyAverageData> AddToReport (List<DailyAverageData> values) {
+            DailyAverageData last = values.Last();
+            MA1 = last.MA1;
+            MA2 = last.MA2;
+            MASignal = last.Signal;
+            return values;
+        }
 
         public List<DailyFloatData> AddToReport (List<DailyFloatData> values) {
             DailyFloatData last = values.Last ();
@@ -44,16 +60,28 @@ namespace Charts {
         }
 
         public string ToCsv () {
-            return $"{Ticker},{Date:yyyy-MM-dd},{Close:F2},{PointSignal},{HighLow:F2},{Reversal:F2},{(PChanged ? "Yes" : "")},{FloatSignal},{Buy:F2},{Sell:F2},{(FChanged ? "Yes" : "")}";
+            return $"{Ticker},{Date:yyyy-MM-dd},{Close:F2},{PointSignal},{HighLow:F2},{Reversal:F2},{(PChanged ? "Yes" : "")},{FloatSignal},{Buy:F2},{Sell:F2},{(FChanged ? "Yes" : "")},{MA1:F2},{MA2:F2},{MASignal},{VolStrength:P2}";
         }
     }
 
     public class DailyReport : List<DailyReportData> {
 
+        private int MANumDays1;
+        private int MANumDays2;
+
+        public DailyReport (int maNumDays1, int maNumDays2) {
+            MANumDays1 = maNumDays1;
+            MANumDays2 = maNumDays2;
+        }
+
+        public string Header() {
+            return $"Ticker,Date,Close,P Signal,High/Low,Reversal,Changed,F Signal,Buy,Sell,Changed,MA({MANumDays1}),MA({MANumDays2}),M Signal,Vol Strength";
+        }
+
         public void OutputAsCsv (string filePath) {
             try {
                 using StreamWriter file = new StreamWriter (filePath);
-                file.WriteLine (DailyReportData.Header);
+                file.WriteLine (this.Header ());
                 foreach (DailyReportData value in this) {
                     file.WriteLine (value.ToCsv ());
                 }
